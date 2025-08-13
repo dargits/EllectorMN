d74a63fa7a46f5377e35510644302bc104d2b2c1
Hoàng Quý Ngọc <148457793+qngocc@users.noreply.github.com>
2025-08-13 18:19:55 +0700

    Add files via upload

--- ElectorApp/Forms/Login.cs
+++ ElectorApp/Forms/Login.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ElectorApp.Services;
+using ElectorApp.Services.AuthService.cs;
 namespace ElectorApp.Forms
 {
     public partial class Login : Form
@@ -22,6 +23,45 @@ namespace ElectorApp.Forms
             Register registerForm = new Register();
             registerForm.Show();
             this.Hide();
+        }
+        private async void click_login(object sender, EventArgs e)
+        {
+            string Account = account.Text.Trim();
+            string Password = password.Text.Trim();
+
+            if (string.IsNullOrEmpty(Account) || string.IsNullOrEmpty(Password))
+            {
+                message.Text = "Vui lòng nhập đủ các trường có đánh dấu '*'.";
+
+                await Task.Delay(3000);
+                message.ForeColor = Color.Red;
+                message.Text = "";
+                return;
+            }
+
+            AuthService AuthService = new AuthService();
+
+            string mess = AuthService.Login(Account, Password);
+            if (mess.Equals("Đăng Nhập Thành Công."))
+                message.ForeColor = Color.Green;
+            else
+                message.ForeColor = Color.Red;
+
+            message.Text = mess;
+            await Task.Delay(3000);
+            message.Text = "";
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Đảm bảo rằng ứng dụng thoát hoàn toàn
+            Application.Exit();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
