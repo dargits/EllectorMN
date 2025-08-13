475397814ad992a8facba71c5a486dcc7c17a33d
Inc Shy <imshydev@gmail.com>
2025-08-13 14:02:55 +0700

    add user model

---
+++ ElectorApp/Forms/User/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectorApp.Models.User;
+
+public class User
+{
+    public int ID { get; set; }
+    public string Account { get; set; }
+    public bool IsAdmin { get; set; }
+    public string Password { get; set; }
+    public String Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public User() { }
+
+    public User(int iD, string account, bool isAdmin, String userName, string password)
+    {
+        ID = iD;
+        Account = account;
+        IsAdmin = isAdmin;
+        Name = userName;
+        Password = password;
+    }
+}
--- ElectorApp/obj/Debug/net8.0-windows/ElectorApp.AssemblyInfo.cs
+++ ElectorApp/obj/Debug/net8.0-windows/ElectorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElectorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6705f2e469e5c5522b7538c917599f3b76bb19e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1da80b75b7e2cc7650261a3c6751d440b3418c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElectorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElectorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
